a018e050fbfe66854753fdc2b443f312ec6a5df3
vydang123 <140564196+vydang123@users.noreply.github.com>
2023-09-01 13:13:40 +1000

    “Week 4 Tutorial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
@@ -14,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+    { username: 'user3', password: 'password3' }
+  ];
+
+  constructor(private router: Router) {}
+
+  login(): void {
+    const user = this.users.find(u => u.username === this.email && u.password === this.password);
+    if (user) {
+      // Successful login, navigate to account page
+      this.router.navigate(['/account']);
+    } else {
+      // Invalid login, show error message
+      alert('Invalid credentials. Please try again.');
+    }
+  }
 }
