67284f7269817d4f5564ce58002c58d913093a4d
vydang123 <140564196+vydang123@users.noreply.github.com>
2023-09-01 12:30:46 +1000

    Week 4 Tutorial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
